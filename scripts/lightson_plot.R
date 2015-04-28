70da163870f059b29d0c8dc66551d07131472fe8
Courtney Campany <courtneycampany@gmail.com>
2015-04-28 10:31:28 +1000

    rearranged script for lights on plotting

--- scripts/lightson_plot.R
+++ scripts/lightson_plot.R
@@ -11,29 +11,32 @@ Ci_bar <- read.csv("calculated_data/Ci_bar.csv")
 gm_drought <- gmes[gmes$drydown == "drought",]
 gm_water <- gmes[gmes$drydown == "control",]
 
+####  mixed effect model for lights on---------------------------------------------------------------------------------
+library(lme4)
+library(lmerTest)
 
-#plot low vs high light shade leaves-----------------------------------------------------------------------------
+gmwater_agg <- summaryBy(Photo +gm ~ id+Month+temp+drydown+chamber+leaflight, data=gm_water,FUN=mean, keep.names=TRUE)
+gm_water_agg2 <- gmwater_agg[gmwater_agg$leaflight != "sun-high",]  
+gm_water_agg2$rowseq <- seq(1:nrow(gm_water_agg2))
+gm_water_agg2 <- droplevels(gm_water_agg2) 
 
-##water
-plot(Photo~gm, data=gm_water, subset=leaflight=="shade-high", pch=16, col=lightscol, ylim=c(0,30), xlim=c(0,.4), 
-     xlab=gmlab, ylab="", cex=1.25)
-points(Photo~gm, data=gm_water, subset=leaflight=="shade-low",pch=16, col=shacol, cex=1.25)
-legend("topright", lightleg, pch=16,inset = 0.02, col=lightlab) 
-legend("topleft", "Shade Leaves (well watered)", bty='n') 
-title(ylab=satlab, mgp=ypos, cex=1.2)
+rowrm <- c(9,36,39,102)
+gm_water_agg3 <- gm_water_agg2[! gm_water_agg2$rowseq %in% c(9, 36,39, 102),] 
+gm_water_sp <- dlply(gm_water_agg3, .(leaflight), function(x) c(x$pairid <- seq(1:nrow(x)), return(x)))
 
-##drought
-plot(Photo~gm, data=gm_drought, subset=leaflight=="shade-high", pch=16, col=lightscol, ylim=c(0,30), xlim=c(0,.4), 
-     xlab=gmlab, ylab="", cex=1.25)
-points(Photo~gm, data=gm_drought, subset=leaflight=="shade-low",pch=16, col=shacol, cex=1.25)
-legend("topright", lightleg, pch=16,inset = 0.02, col=lightlab) 
-legend("topleft", "Shade Leaves (drought)", bty='n') 
-title(ylab=satlab, mgp=ypos, cex=1.2)
+gm_water_agg4 <- rbind.fill(gm_water_sp) 
+gm_water_agg4$pairid2 <- as.factor(paste("a", gm_water_agg4$pairid, sep="-"))
+
+#run model with id (pairid) used as random effect
+lightson_mod <-  lmer(Photo~ gm + (gm|pairid2), data=gm_water_agg4)
 
 
-#####remake plots with lines between paired points---------------------------------------------------------------
+summary(lightson_mod)
 
-##split by light with only data needed
+
+#####plots with lines between paired points---------------------------------------------------------------
+
+##split by light (dfr for drought and watered shade leaves)
 
 wet_ss <- gm_water[, c("chamber", "id", "Photo", "gm", "leaf", "light", "temp", "drydown", "leaflight", "Month")]
 wet_ss2 <- wet_ss[wet_ss$leaf == "shade",]
@@ -66,14 +69,15 @@ dry_low<- data.frame(dry_sp[2])
 
 ##merge by pair
 wet_pair <- merge(wet_high_agg, wet_low_agg, by=c("chamber","Month"), all=TRUE)
-  wet_pair2 <- wet_pair[complete.cases(wet_pair),]
-  wet_pair2 <- arrange(wet_pair, gm.low)
-    wet_pair3 <- addtrt_func(wet_pair2)
+  #wet_pair2 <- wet_pair[complete.cases(wet_pair),]
+  wet_pair2 <- na.omit(wet_pair)
+  wet_pair2 <- arrange(wet_pair2, gm.low)
+  wet_pair3 <- addtrt_func(wet_pair2)
 
 dry_pair <- merge(dry_high_agg, dry_low_agg, by=c("chamber","Month"), all=TRUE)
-  dry_pair2 <- dry_pair[complete.cases(dry_pair),]
-  dry_pair2 <- arrange(dry_pair, gm.low)
-    dry_pair3 <- addtrt_func(dry_pair2)   
+  dry_pair2 <- na.omit(dry_pair)
+  dry_pair2 <- arrange(dry_pair2, gm.low)
+  dry_pair3 <- addtrt_func(dry_pair2)   
     
 ##split for lines
 wetpair_sp <- split(wet_pair2, cumsum(1:nrow(wet_pair2)))
@@ -92,12 +96,11 @@ dry_et <- dry_pair3[dry_pair3$temp == "elevated",]
   dry_at_sp <- split(dry_at, cumsum(1:nrow(dry_at)))
   dry_et_sp <- split(dry_et, cumsum(1:nrow(dry_et)))
 
-
 ###Well watered
 windows(10,8)
-plot(Photo.high~gm.high, data=wet_pair2,  pch=16, col=lightscol, ylim=c(0,30), xlim=c(0,.3), 
+plot(Photo.high~gm.high, data=wet_pair3,  pch=16, col=lightscol, ylim=c(0,30), xlim=c(0,.3), 
      xlab=gmlab, ylab="", cex=1.5)
-  points(Photo.low~gm.low, data=wet_pair2,pch=16, col=shacol, cex=1.5)
+  points(Photo.low~gm.low, data=wet_pair3,pch=16, col=shacol, cex=1.5)
 
 #l_ply(light_sp,  function(x) lines(x=c(x$gm.low,x$gm.high) , y=c(x$Photo.low,x$Photo.high),
 #                    type='l', col="black", lty=1, lwd=1))
@@ -122,27 +125,24 @@ l_ply(wet_et_sp,  function(x) lines(x=c(x$gm.low,x$gm.high) , y=c(x$Photo.low,x$
   
   
   
-####  mixed effect model to test if lights on
-  library(lme4)
-  library(lmerTest)
 
-  gmwater_agg <- summaryBy(Photo +gm ~ id+Month+temp+drydown+chamber+leaflight, data=gm_water,FUN=mean, keep.names=TRUE)
-  gm_water_agg2 <- gmwater_agg[gmwater_agg$leaflight != "sun-high",]  
-  gm_water_agg2$rowseq <- seq(1:nrow(gm_water_agg2))
-  gm_water_agg2 <- droplevels(gm_water_agg2) 
+  #plot low vs high light shade leaves-----------------------------------------------------------------------------
   
-  rowrm <- c(9,36,39,102)
-  gm_water_agg3 <- gm_water_agg2[! gm_water_agg2$rowseq %in% c(9, 36,39, 102),] 
-  gm_water_sp <- dlply(gm_water_agg3, .(leaflight), function(x) c(x$pairid <- seq(1:nrow(x)), return(x)))
-
-  gm_water_agg4 <- rbind.fill(gm_water_sp) 
-  gm_water_agg4$pairid2 <- as.factor(paste("a", gm_water_agg4$pairid, sep="-"))
-
-  #run model with id (pairid) used as random effect
-  lightson_mod <-  lmer(Photo~ gm + (gm|pairid2), data=gm_water_agg4)
-
-
-summary(lightson_mod)
-
+  ##water
+  plot(Photo~gm, data=gm_water, subset=leaflight=="shade-high", pch=16, col=lightscol, ylim=c(0,30), xlim=c(0,.4), 
+       xlab=gmlab, ylab="", cex=1.25)
+  points(Photo~gm, data=gm_water, subset=leaflight=="shade-low",pch=16, col=shacol, cex=1.25)
+  legend("topright", lightleg, pch=16,inset = 0.02, col=lightlab) 
+  legend("topleft", "Shade Leaves (well watered)", bty='n') 
+  title(ylab=satlab, mgp=ypos, cex=1.2)
+  
+  ##drought
+  plot(Photo~gm, data=gm_drought, subset=leaflight=="shade-high", pch=16, col=lightscol, ylim=c(0,30), xlim=c(0,.4), 
+       xlab=gmlab, ylab="", cex=1.25)
+  points(Photo~gm, data=gm_drought, subset=leaflight=="shade-low",pch=16, col=shacol, cex=1.25)
+  legend("topright", lightleg, pch=16,inset = 0.02, col=lightlab) 
+  legend("topleft", "Shade Leaves (drought)", bty='n') 
+  title(ylab=satlab, mgp=ypos, cex=1.2)
+  
   
   
