73d31fffdd77540c826683d330b8364f91b82ae2
Courtney Campany <courtneycampany@gmail.com>
2015-06-01 15:57:34 +1000

    cex sizes of png plots

--- makepngs/sunshade_phys_png.R
+++ makepngs/sunshade_phys_png.R
@@ -67,23 +67,22 @@ gm_c13 <- merge(gm_sunsha, Ci_bar[, c(2,8)], by="id")
   ###plot
   png(filename = "makepngs/photo_cond.png", width = 11, height = 8.5, units = "in", res= 400)
   
-  par(mar=c(5,5,2,2), cex.lab=1.25)
+  par(mar=c(5,7,2,2), cex.lab=2)
   plot(Photo~Cond, data=gm_c13, subset=leaflight=="sun-high", pch=16, col=suncol, ylim=c(0,25), 
-       xlim=c(0,.4), xlab=condlab, ylab="", cex=1.5)
+       xlim=c(0,.4), xlab="", ylab=satlab, cex.axis=1.75, cex=1.5)
   
-  lines(gssun_seq, sunupr, lty=2, lwd=2,col=suncol)
-  lines(gssun_seq, sunlwr, lty=2, lwd=2,col=suncol)
-  lines(gssun_seq, gssun_pred$fit, lty=1, lwd=2,col=suncol)
+  lines(gssun_seq, sunupr, lty=2, lwd=3.5,col=suncol)
+  lines(gssun_seq, sunlwr, lty=2, lwd=3.5,col=suncol)
+  lines(gssun_seq, gssun_pred$fit, lty=1, lwd=3.5,col=suncol)
   
   #shade
   points(Photo~Cond, data=gm_c13, subset=leaflight=="shade-low", pch=16, col=shacol, cex=1.5)
-  lines(gssha_seq, shaupr, lty=2, lwd=2,col=shacol)
-  lines(gssha_seq, shalwr, lty=2, lwd=2,col=shacol)
-  lines(gssha_seq, gssha_pred$fit, lty=1, lwd=2,col=shacol)
+  lines(gssha_seq, shaupr, lty=2, lwd=3.5,col=shacol)
+  lines(gssha_seq, shalwr, lty=2, lwd=3.5,col=shacol)
+  lines(gssha_seq, gssha_pred$fit, lty=1, lwd=3.5,col=shacol)
   
-
-  title(ylab=satlab, mgp=ypos, cex=1.25)
-  legend("topleft", leaflab2, pch=16,inset = 0.03, col=leafcol, bty='n', cex=1.25) 
+  title(xlab=condlab, line=4, cex=1.25)
+  legend("topleft", leaflab2, pch=16,inset = 0.02, col=leafcol, bty='n', cex=1.75) 
   
   dev.off()
 
@@ -133,22 +132,23 @@ gm_c13 <- merge(gm_sunsha, Ci_bar[, c(2,8)], by="id")
   
   png(filename = "makepngs/photo_cc.png", width = 11, height = 8.5, units = "in", res= 400)
   
-  par(mar=c(5,5,2,2), cex.lab=1.25)
-  plot(Photo~Cc, data=sundat, pch=16, col=suncol, ylim=c(0,25), xlim=c(0,350), xlab=cclab, ylab="", cex=1.5)
+  par(mar=c(5,7,2,2), cex.lab=2)
+  plot(Photo~Cc, data=sundat, pch=16, col=suncol, ylim=c(0,25), xlim=c(0,350), xlab="", ylab=satlab, 
+       cex.axis=1.75, cex=1.5)
   points(Photo~Cc, data=gm_c13, subset=leaflight=="shade-low", pch=16, col=shacol, cex=1.5)
   with(acc_sun, {
-    lines(Cc, lcl, lty=2, lwd=2,col="forestgreen")
-    lines(Cc, ucl, lty=2, lwd=2,col="forestgreen")
-    lines(Cc, pred, lty=1, lwd=2,col="forestgreen")
+    lines(Cc, lcl, lty=2, lwd=3.5,col="forestgreen")
+    lines(Cc, ucl, lty=2, lwd=3.5,col="forestgreen")
+    lines(Cc, pred, lty=1, lwd=3.5,col="forestgreen")
   })
   with(acc_sha, {
-    lines(Cc, lcl, lty=2, lwd=2,col="yellow4")
-    lines(Cc, ucl, lty=2, lwd=2,col="yellow4")
-    lines(Cc, pred, lty=1, lwd=2,col="yellow4")
+    lines(Cc, lcl, lty=2, lwd=3.5,col="yellow4")
+    lines(Cc, ucl, lty=2, lwd=3.5,col="yellow4")
+    lines(Cc, pred, lty=1, lwd=3.5,col="yellow4")
   })
   
-  title(ylab=satlab, mgp=ypos, cex=1.25)
-  legend("topright", leaflab2, pch=16,inset = 0.03, col=leafcol,bty='n', cex=1.25) 
+  title(xlab=cclab, line=4, cex=1.25)
+  legend("topleft", leaflab2, pch=16,inset = 0.02, col=leafcol, bty='n', cex=1.75) 
 
   dev.off()
   
@@ -174,21 +174,23 @@ aci_sha <- arrange(aci_sha, Ci)
 
 png(filename = "makepngs/photo_ci.png", width = 11, height = 8.5, units = "in", res= 400)
 
-par(mar=c(5,5,2,2), cex.lab=1.25)
-plot(Photo~Ci, data=sundat, pch=16, col=suncol, ylim=c(0,25), xlim=c(0,350), xlab=cilab, ylab="", cex=1.5)
+par(mar=c(5,7,2,2), cex.lab=2)
+plot(Photo~Ci, data=sundat, pch=16, col=suncol, ylim=c(0,25), xlim=c(0,350), xlab="", ylab=satlab, 
+     cex.axis=1.75,cex=1.5)
 points(Photo~Ci, data=gm_c13, subset=leaflight=="shade-low", pch=16, col=shacol, cex=1.5)
 with(aci_sun, {
-  lines(Ci, lcl, lty=2, lwd=2,col="forestgreen")
-  lines(Ci, ucl, lty=2, lwd=2,col="forestgreen")
-  lines(Ci, pred, lty=1, lwd=2,col="forestgreen")
+  lines(Ci, lcl, lty=2, lwd=3.5,col="forestgreen")
+  lines(Ci, ucl, lty=2, lwd=3.5,col="forestgreen")
+  lines(Ci, pred, lty=1, lwd=3.5,col="forestgreen")
 })
 with(aci_sha, {
-  lines(Ci, lcl, lty=2, lwd=2,col="yellow4")
-  lines(Ci, ucl, lty=2, lwd=2,col="yellow4")
-  lines(Ci, pred, lty=1, lwd=2,col="yellow4")
+  lines(Ci, lcl, lty=2, lwd=3.5,col="yellow4")
+  lines(Ci, ucl, lty=2, lwd=3.5,col="yellow4")
+  lines(Ci, pred, lty=1, lwd=3.5,col="yellow4")
 })
-title(ylab=satlab, mgp=ypos, cex=1.25)
-legend("topright", leaflab2, pch=16,inset = 0.03, col=leafcol,bty='n', cex=1.25) 
+
+title(xlab=cilab, line=4, cex=1.25)
+legend("topleft", leaflab2, pch=16,inset = 0.02, col=leafcol, bty='n', cex=1.75)
 
 dev.off()
 
@@ -213,22 +215,25 @@ Agm_sha_lm <- lmer(Photo~ gm + (1|chamber), data=agm_sha)
   agm_sha <- arrange(agm_sha, gm)
 
   
-  png(filename = "makepngs/photo_gm.png", width = 11, height = 8.5, units = "in", res= 400)
-  par(mar=c(5,5,2,2), cex.lab=1.25)
-plot(Photo~gm, data=sundat, pch=16, col=suncol, ylim=c(0,25), xlim=c(0,.5), xlab=gmlab, ylab="", cex=1.5)
+png(filename = "makepngs/photo_gm.png", width = 11, height = 8.5, units = "in", res= 400)
+
+par(mar=c(5,7,2,2), cex.lab=2)
+plot(Photo~gm, data=sundat, pch=16, col=suncol, ylim=c(0,25), xlim=c(0,.5), xlab="", ylab=satlab, 
+     cex.axis=1.75, cex=1.5)
   points(Photo~gm, data=gm_c13, subset=leaflight=="shade-low", pch=16, col=shacol, cex=1.5)
 with(agm_sun, {
-  lines(gm, lcl, lty=2, lwd=2,col="forestgreen")
-  lines(gm, ucl, lty=2, lwd=2,col="forestgreen")
-  lines(gm, pred, lty=1, lwd=2,col="forestgreen")
+  lines(gm, lcl, lty=2, lwd=3.5,col="forestgreen")
+  lines(gm, ucl, lty=2, lwd=3.5,col="forestgreen")
+  lines(gm, pred, lty=1, lwd=3.5,col="forestgreen")
 })
 with(agm_sha, {
-  lines(gm, lcl, lty=2, lwd=2,col="yellow4")
-  lines(gm, ucl, lty=2, lwd=2,col="yellow4")
-  lines(gm, pred, lty=1, lwd=2,col="yellow4")
+  lines(gm, lcl, lty=2, lwd=3.5,col="yellow4")
+  lines(gm, ucl, lty=2, lwd=3.5,col="yellow4")
+  lines(gm, pred, lty=1, lwd=3.5,col="yellow4")
 })
-title(ylab=satlab, mgp=ypos, cex=1.25)
-legend("topright", leaflab2, pch=16,inset = 0.03, col=leafcol,bty='n', cex=1.25) 
+
+title(xlab=gmlab, line=4, cex=1.25)
+legend("topleft", leaflab2, pch=16,inset = 0.02, col=leafcol, bty='n', cex=1.75) 
 
 dev.off()
 
@@ -254,24 +259,25 @@ dev.off()
   
 
 png(filename = "makepngs/photo_cibar.png", width = 11, height = 8.5, units = "in", res= 400)
-par(mar=c(5,5,2,2), cex.lab=1.25)
+par(mar=c(5,7,2,2), cex.lab=2)
 
 plot(Photo~ci_bar, data=sundat, pch=16, col=suncol, ylim=c(0,25), 
-     xlim=c(150,350), xlab=cibarlab2, ylab="", cex=1.5)
+     xlim=c(150,350), xlab="", ylab=satlab ,cex.axis=1.75, cex=1.5)
 #shade
 points(Photo~ci_bar, data=shadat, pch=16, col=shacol, cex=1.5)
   with(acib_sun, {
-    lines(ci_bar, lcl, lty=2, lwd=2,col="forestgreen")
-    lines(ci_bar, ucl, lty=2, lwd=2,col="forestgreen")
-    lines(ci_bar, pred, lty=1, lwd=2,col="forestgreen")
+    lines(ci_bar, lcl, lty=2, lwd=3.5,col="forestgreen")
+    lines(ci_bar, ucl, lty=2, lwd=3.5,col="forestgreen")
+    lines(ci_bar, pred, lty=1, lwd=3.5,col="forestgreen")
   })
   with(acib_sha, {
-    lines(ci_bar, lcl, lty=2, lwd=2,col="yellow4")
-    lines(ci_bar, ucl, lty=2, lwd=2,col="yellow4")
-    lines(ci_bar, pred, lty=1, lwd=2,col="yellow4")
+    lines(ci_bar, lcl, lty=2, lwd=3.5,col="yellow4")
+    lines(ci_bar, ucl, lty=2, lwd=3.5,col="yellow4")
+    lines(ci_bar, pred, lty=1, lwd=3.5,col="yellow4")
   })
-title(ylab=satlab, mgp=ypos, cex=1.25)
-legend("topleft", leaflab2, pch=16,inset = 0.03, col=leafcol,bty='n', cex=1.25) 
+
+title(xlab=cibarlab2, line=4, cex=1.25)
+legend("topleft", leaflab2, pch=16,inset = 0.02, col=leafcol, bty='n', cex=1.75) 
 
 dev.off()
   
@@ -308,21 +314,22 @@ dev.off()
   
   ###plot
   png(filename = "makepngs/photo_gmgs.png", width = 11, height = 8.5, units = "in", res= 400)
-  par(mar=c(5,5,2,2), cex.lab=1.25)
+  par(mar=c(5,7,2,2), cex.lab=2)
   
   plot(Photo~gmgs, data=gm_c13, subset=leaflight=="sun-high", pch=16, col=suncol, ylim=c(0,25), 
-       xlim=c(0,.6), xlab=condlab, ylab="", cex=1.5)
+       xlim=c(0,.6), xlab="", ylab=satlab, cex.axis=1.75, cex=1.5)
   
-  lines(gmgs_sun_seq, gmgs_sunupr, lty=2, lwd=2,col=suncol)
-  lines(gmgs_sun_seq, gmgs_sunlwr, lty=2, lwd=2,col=suncol)
-  lines(gmgs_sun_seq, gmgs_sun_pred$fit, lty=1, lwd=2,col=suncol)
+  lines(gmgs_sun_seq, gmgs_sunupr, lty=3.5, lwd=2,col=suncol)
+  lines(gmgs_sun_seq, gmgs_sunlwr, lty=3.5, lwd=2,col=suncol)
+  lines(gmgs_sun_seq, gmgs_sun_pred$fit, lty=1, lwd=3.5,col=suncol)
   
   #shade
   points(Photo~gmgs, data=gm_c13, subset=leaflight=="shade-low", pch=16, col=shacol, cex=1.5)
-  lines(gmgs_sha_seq, gmgs_shaupr, lty=2, lwd=2,col=shacol)
-  lines(gmgs_sha_seq, gmgs_shalwr, lty=2, lwd=2,col=shacol)
-  lines(gmgs_sha_seq, gmgs_sha_pred$fit, lty=1, lwd=2,col=shacol)
-  title(ylab=satlab, mgp=ypos, cex=1.25)
-  legend("topleft", leaflab2, pch=16,inset = 0.03, col=leafcol,bty='n', cex=1.25) 
+  lines(gmgs_sha_seq, gmgs_shaupr, lty=2, lwd=3.5,col=shacol)
+  lines(gmgs_sha_seq, gmgs_shalwr, lty=2, lwd=3.5,col=shacol)
+  lines(gmgs_sha_seq, gmgs_sha_pred$fit, lty=1, lwd=3.5,col=shacol)
+  
+  title(xlab=totcondlab, line=4, cex=1.25)
+  legend("topleft", leaflab2, pch=16,inset = 0.02, col=leafcol, bty='n', cex=1.75)  
 
 dev.off()
