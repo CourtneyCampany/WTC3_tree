ae9f9edf8f4852e3bf767e57b04b7542da0ace6e
Courtney Campany <courtneycampany@gmail.com>
2015-06-26 17:12:56 +1000

    determined relationship between gm and temp
    fixed par figure so that it doesnt reflect drought
    
    delted bar plots because made a new script
    
    moved more scripts out of master folder

--- master_scripts/par.R
+++ master_scripts/par.R
@@ -1,19 +1,20 @@
-# source("functions and packages/functions.R")
-# source("functions and packages/packages.R")
-# source("master_scripts/plot_objects.R")
-# 
-# par <- read.csv("raw data/par.csv")
-# 
-# treatments <- read.csv("raw data/temp_trt.csv")
+source("functions and packages/functions.R")
+source("functions and packages/packages.R")
+source("master_scripts/plot_objects.R")
+
+par <- read.csv("raw data/par.csv")
+
+treatments <- read.csv("raw data/temp_trt.csv")
 
 #format function
 par<- parformat(par)
 
 par_leaf <- subset(par, ID !="shade-high")
+par_leaf2 <- par_leaf[par_leaf$drydown == "control",]
 Morder <- c("Oct", "Dec", "Jan", "Feb", "Mar", "Apr")
 
 #data format for bar
-parbar <- par_leaf[ , c(2:3,5)]
+parbar <- par_leaf2[ , c(2:3,5)]
 parbar$month <- factor(parbar$month, levels = Morder)
 levels(parbar$month)
 parbar$leaf_type <- gsub("s", "S", parbar$leaf_type)
@@ -21,29 +22,30 @@ parbar$leaf_type <- gsub("s", "S", parbar$leaf_type)
 par_agg <- summaryBy(par ~ leaf_type, data=parbar, FUN=mean)
 
 ###Stats---------------------------------------------------------------------------------------------------------
-# library(visreg)
-# library(multcomp)
-# library(nlme)
+library(visreg)
+library(multcomp)
+library(nlme)
 
 #examite data with boxplots, then remove any outliers
-# boxplot(par~temp, data=par_leaf[par_leaf$leaf_type =="sun",])
-# boxplot(par~temp, data=par_leaf[par_leaf$leaf_type =="shade",])
+boxplot(par~temp, data=par_leaf2[par_leaf2$leaf_type =="sun",])
+boxplot(par~temp, data=par_leaf2[par_leaf2$leaf_type =="shade",])
 
 
-###lma not different between leaf types or temp treatment.
-# par_leaf <- lme(par ~ leaf_type, random=~1|chamber, data=par_leaf)
-# summary(par_leaf)
-# anova(par_leaf)
+##PAR significantly different between leaves not by temperature treatment
+par_mod <- lme(par ~ leaf_type, random=~1|chamber, data=par_leaf2)
+summary(par_mod)
+anova(par_mod)
 
+# par_mod2 <- lme(par ~ leaf_type*temp, random=~1|chamber, data=par_leaf2)
+# anova(par_mod2)
 
 ###PLOTTING------------------------------------------------------------------------------------------------
 
-#windows(7,5)
+windows(8,6)
 bar(par, c(leaf_type, month), parbar, col=c(shacol,suncol), xlab="", ylab=parlab, ylim=c(0, 2000), 
       half.errbar=FALSE, mar=c(5,5,2,2))
-#title(ylab=parlab, mgp=ypos)
-# dev.copy2pdf(file="master_scripts/figures/ppfd.pdf")
-# dev.off()
+ dev.copy2pdf(file="master_scripts/paper_figures/ppfd.pdf")
+ dev.off()
 
 
 ###for png
--- stats/gm_temp.R
+++ stats/gm_temp.R
@@ -1,32 +1,49 @@
 ### read in gm and test if different by temp
 
-gmes <- read.csv("calculated_data/gmes_WTC.csv")
+gmes <- read.csv("calculated_data/gmes_wellwatered.csv")
 
-###for analysis first subset well watered and drought treatments
-gm_drought <- gmes[gmes$drydown == "drought",]
-gm_water <- gmes[gmes$drydown == "control",]
+###get average by id
+gm_agg <- summaryBy(Photo+ CTleaf+gm ~ chamber+id+leaf +light+temp+leaflight+Month, data=gmes, FUN=mean, keep.names=TRUE)
 
+library(visreg)
+library(multcomp)
+library(nlme)
+
+##linear model with chamber as random effect
+
+gmt_sun_mod <- lme(gm~ CTleaf ,random=~1|chamber, data=gm_agg, subset=leaflight=="sun-high")
+gmt_sha_mod <- lme(gm~ CTleaf ,random=~1|chamber, data=gm_agg,subset=leaflight=="shade-low")
+gmt_fleck_mod <- lme(gm~ CTleaf ,random=~1|chamber, data=gm_agg,subset=leaflight=="shade-high")
+
+  #sun leaves
+  summary(gmt_sun_mod)
+  anova(gmt_sun_mod)
+  visreg(gmt_sun_mod)
+  #shade leaves
+  summary(gmt_sha_mod)
+  anova(gmt_sha_mod)
+  visreg(gmt_sha_mod)
+  #sunfleck leaves
+  summary(gmt_fleck_mod)
+  anova(gmt_fleck_mod)
+  visreg(gmt_fleck_mod)
+  
+##full model with temperature treatment
 library(lme4)
+sun_mod2 <- lmer(gm~ CTleaf * temp + (1|chamber), data=gm_agg, subset=leaflight =="sun-high")
+  summary(sun_mod2)
+  anova(sun_mod2)
+  visreg(sun_mod2)
 
-gmt_sun_wet <- lmer(gm~ CTleaf + (1|chamber), data=gm_water, subset=leaflight=="sun-high")
-gmt_sha_wet <- lmer(gm~ CTleaf + (1|chamber), data=gm_water,subset=leaflight=="sun-high")
-gmt_sun_dry <- lmer(gm~ CTleaf + (1|chamber), data=gm_drought,subset=leaflight=="shade-low")
-gmt_sha_dry <- lmer(gm~ CTleaf + (1|chamber), data=gm_drought,subset=leaflight=="shade-low")
+sha_mod2 <- lmer(gm~ CTleaf * temp + (1|chamber), data=gm_agg, subset=leaflight =="shade-low")
+  summary(sha_mod2)
+  anova(sha_mod2)
+  visreg(sha_mod2)
 
-test <- lmer(gm~ CTleaf + (1|chamber), data=gm_water)
-
-###summary
-summary(gmt_sun_wet)
-gmt_sun_wet2 <- update(gmt_sun_wet, REML=FALSE)
-gmt_sun_wet3 <- lm(gm~ CTleaf, data=gm_water, subset=leaflight=="sun-high")
+fleck_mod2 <- lmer(gm~ CTleaf * temp + (1|chamber), data=gm_agg, subset=leaflight =="shade-high")
+  summary(fleck_mod2)
+  anova(fleck_mod2)
+  visreg(fleck_mod2)
 
 
-plot(gm~CTleaf, data=gm_water,col=leaflight, pch=16)
-  plot(gm~CTleaf, data=gm_water,subset=leaflight=="sun-high", col=leaflight, pch=16)
-  plot(gm~CTleaf, data=gm_water,subset=leaflight=="shade-low",col=leaflight, pch=16)
-  plot(gm~CTleaf, data=gm_water,subset=leaflight=="shade-high",col=leaflight, pch=16)
 
-plot(gm~CTleaf, data=gm_drought, col=leaflight, pch=16)
-  plot(gm~CTleaf, data=gm_drought,subset=leaflight=="sun-high", col=leaflight, pch=16)
-  plot(gm~CTleaf, data=gm_drought,subset=leaflight=="shade-low",col=leaflight, pch=16)
-  plot(gm~CTleaf, data=gm_drought,subset=leaflight=="shade-high",col=leaflight, pch=16)
