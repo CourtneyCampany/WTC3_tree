61bf17650fef2b5aa45c0d012b4192ccf5930804
Courtney Campany <courtneycampany@gmail.com>
2016-03-03 17:36:34 +1100

    reworked aci parameters to acc parameters.  aci parameters now redundant
    updated data table so far with stats

---
+++ master_scripts/acc_vcmaxnitro.R
@@ -0,0 +1,169 @@
+source("functions and packages/packages.R")
+source("functions and packages/functions.R")
+source("master_scripts/plot_objects.R")
+
+treatments <- read.csv("raw data/temp_trt.csv") 
+
+###Photosythesis data-----------------------------------------------------------------------------------------------
+photo <- read.csv("calculated_data/gmes_wellwatered.csv")
+###get average by id
+photo_agg <- summaryBy(Photo ~ chamber+id+leaf +light+temp+leaflight+Month, data=photo, FUN=mean, keep.names=TRUE)
+photo2 <- summaryBy(Photo~ chamber+leaf+temp+leaflight+Month, data=photo_agg, FUN=mean, keep.names=TRUE)
+
+###data for aci curves----------------------------------------------------------------------------------------------  
+  
+  #read ACi datasets 
+acishade <- read.csv("raw data/shadeaci.csv")
+acishade <- merge(acishade, treatments)
+  
+  #clean from previous script
+  acishade_clean <- acishade[acishade$chamber != "ch02",]
+  acishade_clean <- acishade_clean[acishade_clean$chamber != "ch07",]
+  acishade_clean <- acishade_clean[acishade_clean$chamber != "ch09",]
+  acishade_clean <- acishade_clean[acishade_clean$chamber != "ch11",]
+
+  shade_redo <- read.csv("raw data/shadeaci_redo.csv")
+  shade_redo <- merge(shade_redo, treatments)
+  
+  sunaci <- read.csv("raw data/sunaci.csv")
+  sunaci <-merge(sunaci, treatments)
+    
+  #clean from previous script
+  sunaci_clean <- sunaci[sunaci$chamber != "ch06", ]
+  sunaci_clean2 <- sunaci_clean[sunaci_clean$chamber != "ch04", ]
+  
+  tdlaci2 <- read.csv("raw data/tdlaci2.csv")
+  tdlaci2 <-merge(tdlaci2, treatments)
+  
+  #read in jmax vcmax
+  jvc<- read.csv("calculated_data/tdl_aci.csv")
+  
+  #simulate ACi curves at 1800 par
+  sunAT_sim2 <- Aci(Ci=seq(50,2000,length=101), Vcmax=jvc[3,3], Jmax=jvc[3,4],PPFD=1800)
+  sunET_sim2 <- Aci(Ci=seq(50,2000,length=101), Vcmax=jvc[4,3], Jmax=jvc[4,4], PPFD=1800)
+  shaAT_sim2 <- Aci(Ci=seq(50,2000,length=101), Vcmax=jvc[1,3], Jmax=jvc[1,4], PPFD=1800)
+  shaET_sim2 <- Aci(Ci=seq(50,2000,length=101), Vcmax=jvc[2,3], Jmax=jvc[2,4],PPFD=1800)
+  
+
+###Narea data------------------------------------------------------------------------------------------------------
+leaf_chem <- read.csv("raw data/leaf_chem.csv")
+leaf_mass <- read.csv("raw data/leaf_data.csv")
+  
+#get vcmax per chamber 
+aciparam <- read.csv("calculated_data/aciparameters_gm.csv")
+  
+#calculate nitrogen content in leaves
+canopy_chem <- merge(leaf_mass[leaf_mass$wp_type=="mid",c(1, 3:4, 7:8)], leaf_chem[, 4:8], by=c("Month", "chamber", "leaf"))
+  #add treatments  
+  canopy_chem <- addtrt_func(canopy_chem)
+  canopy_chem$leafN <- with(canopy_chem, leaf_mass *(n_perc/100))
+  canopy_chem$lma <- with(canopy_chem, (leaf_mass/leaf_area)*10000) #g/m2
+  canopy_chem$leafN_area <- with(canopy_chem, lma *(n_perc/100))
+  canopy_chem<- add_campaign(canopy_chem)
+  
+  canopy_chem2 <- canopy_chem[canopy_chem$drydown=="control",]
+  canopy_chem3 <- canopy_chem2[, c("chamber", "Month", "leaf", "leafN_area")]
+  
+##vcmax and Narea dataset  (with stats for results)----------------------------------------------------------------
+Nagg <- summaryBy(leafN_area ~ chamber + leaf, data=canopy_chem)
+  N_aci <- merge(aciparam, Nagg, by= c("chamber", "leaf"))
+
+# library(nlme)
+# library(lme4)
+# library(lmerTest)
+# library(LMERConvenienceFunctions)
+nitrovc_mod <- lm(Vcmax~leafN_area.mean, data=N_aci)
+# nitrovc_mod2 <- lme(Vcmax~leafN_area.mean, random=~1|chamber,data=N_aci)
+# nitrovc_mod3 <- lmer(Vcmax~leafN_area.mean+ (1|chamber),data=N_aci)
+# anova(nitrovc_mod3)
+# summary(nitrovc_mod3)
+# mcp.fnc(nitrovc_mod3)
+# source("functions and packages/r2glmm.R")
+# rsquared.glmm(nitrovc_mod3)
+# 
+# summary(nitrovc_mod)
+# summary(nitrovc_mod2)
+# anova(nitrovc_mod,nitrovc_mod3)
+
+####photosynthesis and narea dataset (with stats)--------------------------------------------------------------------
+###merge photo with leaf N datasets
+
+Anitro <- merge(photo2, canopy_chem3, by=c("chamber", "Month", "leaf"))  
+
+photoN_mod <- lm(Photo~leafN_area, data=Anitro[Anitro$leaflight != "shade-high",])
+# photoN_mod2 <- lme(Photo~leafN_area, random=~1|chamber,data=Anitro[Anitro$leaflight != "shade-high",])
+# photoN_mod3<- lmer(Photo~leafN_area+ (1|chamber), data=Anitro[Anitro$leaflight != "shade-high",])
+# anova(photoN_mod3)
+# summary(photoN_mod3)
+# mcp.fnc(photoN_mod3)
+# rsquared.glmm(photoN_mod3)
+# 
+# summary(photoN_mod)
+# summary(photoN_mod2)
+
+
+###plotting
+palette(c(shacol, suncol))
+  
+windows(8,10)
+
+par(fig=c(0, 1, .5, 1),mar=c(4,4,1,1), cex=1.25, las=1, cex.axis=.8, cex.lab=1, mgp=c(2.5,1,0))
+  
+#panel 1: aci curves with inset
+plot(Photo~Ci ,data= acishade_clean, col=lights50col, ylim=c(0, 42), xlim=c(0,2000), xlab=cilab, 
+       ylab=photolab, pch=c(16, 17)[pch=acishade_clean$temp])
+  points(Photo~Ci ,data= shade_redo, col=lights50col,  pch=c(16, 17)[pch=shade_redo$temp])
+  points(Photo~Ci ,data= sunaci_clean2,  col=suncol50,  pch=c(16, 17)[pch=sunaci_clean2$temp])
+  points(Photo~Ci ,data= tdlaci2,  col=suncol50,  pch=c(16, 17)[pch=tdlaci2$temp])
+
+  points(sunAT_sim2$Ci, sunAT_sim2$ALEAF, col=suncol2, cex=1.1,xlab=cilab, ylab="", type="l", lwd=2)
+  points(sunET_sim2$Ci, sunET_sim2$ALEAF, col=suncol2, cex=1.1,xlab="", ylab="", type="l", lwd=2, lty=2)
+  points(shaAT_sim2$Ci, shaAT_sim2$ALEAF, col=lightscol2, cex=1.1,xlab="", ylab="", type="l", lwd=2)
+  points(shaET_sim2$Ci, shaET_sim2$ALEAF, col=lightscol2, cex=1.1,xlab="", ylab="", type="l", lwd=2, lty=2)
+  
+  legend("topleft", c("Sun", "Shade-High Light", "AT", "ET"), pch=c(-1,-1, 16,17), lty=c(1,1, 1,2),
+         col=c(suncol, lightscol2, "black", "black"),inset = 0.01, bty='n',cex=.7)
+  text(x=2040, y=42, "(a)", cex=.7)
+  
+###panel 2: leafNarea ~ vcmax (bottom left)
+par(fig=c(0, .5, 0, .5),new=T,mar=c(4,4,1,1), cex=1.25, las=1, cex.axis=.8, cex.lab=1, mgp=c(2.5,1,0))
+
+plot(Vcmax~leafN_area.mean, data=N_aci[N_aci$leaf=="shade",], col=shacol,  ylim=c(40, 133),xlim=c(1,3.5),
+       pch=c(16, 17)[pch=N_aci$temp],xlab=narealab, ylab=vclab)
+  points(Vcmax~leafN_area.mean, data=N_aci[N_aci$leaf=="sun",], col=suncol, pch=c(16, 17)[pch=N_aci$temp])
+
+  ablineclip(nitrovc_mod, x1=min(N_aci$leafN_area.mean), x2=max(N_aci$leafN_area.mean), lwd=2, lty=3)
+  legend("topleft", leglab2, pch=c(16,17,16,17), col=trtcols,inset = 0.01, bty='n',cex=.7)
+  text(x=3.475, y=133, "(b)", cex=.7)
+  
+####panel bottom right 
+par(fig=c(.5, 1, 0, .5),new=T,mar=c(4,4,1,1), cex=1.25, las=1, cex.axis=.8, cex.lab=1, mgp=c(2.5,1,0))
+plot(Photo ~ leafN_area, data=Anitro[Anitro$leaflight=="sun-high",], col=suncol,  xlim=c(0,4), ylim=c(0,25),
+       pch=c(16, 17)[pch=Anitro$temp], ylab=photolab, xlab=narealab)
+  points(Photo ~ leafN_area, data=Anitro[Anitro$leaflight=="shade-low",], col=shacol,  pch=c(16, 17)[pch=Anitro$temp])
+  ablineclip(photoN_mod, x1=min(Anitro$leafN_area), x2=max(Anitro$leafN_area), lwd=2, lty=3)
+  #legend("topleft", leglab2, pch=c(16,17,16,17), col=trtcols,inset = 0.01, bty='n',cex=.8)  
+  text(x=3.95, y=25, "(c)", cex=.7)
+ 
+  
+###inset figure  
+par(fig=c(0.525, 0.95, 0.58,0.75), mar=c(2,2,0,0),new=T, cex=.7, las=1,  cex.axis=.7, cex.lab=.7, tcl=-.25)
+  
+plot(Photo~Ci ,data= acishade_clean, ylim=c(0, 15.5), xlim=c(45,300), xlab="", ylab="",xaxt="n", yaxt="n", pch="")
+  axis(2, mgp=c(3, .5, 0))
+  axis(1, mgp=c(3, 0, 0))
+  
+  points(Photo~Ci ,data= acishade_clean, col=lights50col,pch=c(16, 17)[pch=acishade_clean$temp])
+  points(Photo~Ci ,data= shade_redo,col=lights50col, pch=c(16, 17)[pch=shade_redo$temp])
+  points(Photo~Ci ,data= sunaci_clean2, col=suncol50, pch=c(16, 17)[pch=sunaci_clean2$temp])
+  points(Photo~Ci ,data= tdlaci2, col=suncol50, pch=c(16, 17)[pch=tdlaci2$temp])
+  
+  points(sunAT_sim2$Ci, sunAT_sim2$ALEAF, col=suncol2,  type="l", lwd=2)
+  points(sunET_sim2$Ci, sunET_sim2$ALEAF, col=suncol2,  type="l", lwd=2, lty=2)
+  points(shaAT_sim2$Ci, shaAT_sim2$ALEAF, col=lightscol2,  type="l", lwd=2)
+  points(shaET_sim2$Ci, shaET_sim2$ALEAF, col=lightscol2,  type="l", lwd=2, lty=2)  
+   
+  
+# dev.copy2pdf(file="master_scripts/paper_figures/acc_nitrovcmax.pdf")
+# dev.off()
+  
--- scripts/jmaxvcmax_gmes.R
+++ scripts/jmaxvcmax_gmes.R
@@ -9,27 +9,23 @@ gm_agg <-summaryBy(gm ~ leaflight+temp, data=gm, FUN=mean, keep.names=TRUE)
 #read in temperature treatments
 treatments <- read.csv("raw data/temp_trt.csv") 
 
-##read in aci data
-acishade <- read.csv("raw data/shadeaci.csv")
-acishade <- merge(acishade, treatments)
 
 ##shade leaves-------------------------------------------------------------------------------------------------------------
+acishade <- read.csv("raw data/shadeaci.csv")
+  acishade <- merge(acishade, treatments)
+  
+shade_redo <- read.csv("raw data/shadeaci_redo.csv")
+  shade_redo <- merge(shade_redo, treatments)
 
 #clean from previous script
-acishade_clean <- acishade[acishade$chamber != "ch02",]
-acishade_clean <- acishade_clean[acishade_clean$chamber != "ch07",]
-acishade_clean <- acishade_clean[acishade_clean$chamber != "ch09",]
-acishade_clean <- acishade_clean[acishade_clean$chamber != "ch11",]
+acishade_clean <- acishade[!(acishade$chamber %in% c("ch02","ch07","ch09","ch11")),]
 
-##will have to include temperature treatment so aci data will have to be further subsetted
+##have to seperates temperature treatment so datasets
 acishade_clean_at <- acishade_clean[acishade_clean$temp == "ambient",]
   acishade_clean_at <-droplevels(acishade_clean_at)
 
 acishade_clean_et <- acishade_clean[acishade_clean$temp == "elevated",]
   acishade_clean_et <- droplevels(acishade_clean_et)
-
-shade_redo <- read.csv("raw data/shadeaci_redo.csv")
-  shade_redo <- merge(shade_redo, treatments)
 
 shade_redo_at <- shade_redo[shade_redo$temp == "ambient",]
   shade_redo_at <- droplevels(shade_redo_at)
@@ -40,17 +36,17 @@ shade_redo_et <- shade_redo[shade_redo$temp == "elevated",]
 
 ##sunleaves-------------------------------------------------------------------------------------------------------------------
 sunaci <- read.csv("raw data/sunaci.csv")
-sunaci <-merge(sunaci, treatments)
+  sunaci <-merge(sunaci, treatments)
 
-#clean from previous script
-sunaci_clean <- sunaci[sunaci$chamber != "ch06", ]
-  sunaci_clean2 <- sunaci_clean[sunaci_clean$chamber != "ch04", ]
+#clean from previous script  (lets see if i need this yet)
+# sunaci_clean <- sunaci[sunaci$chamber != "ch06", ]
+#   sunaci_clean2 <- sunaci_clean[sunaci_clean$chamber != "ch04", ]
   
-  sunaci_clean2_at <- sunaci_clean2[sunaci_clean2$temp == "ambient",]
-  sunaci_clean2_at <- droplevels(sunaci_clean2_at)
-  
-  sunaci_clean2_et <- sunaci_clean2[sunaci_clean2$temp == "elevated",]
-  sunaci_clean2_et <- droplevels(sunaci_clean2_et)
+sunaci_at <- sunaci[sunaci$temp == "ambient",]
+  sunaci_at <- droplevels(sunaci_at)
+
+sunaci_et <- sunaci[sunaci$temp == "elevated",]
+  sunaci_et <- droplevels(sunaci_et)
 
   
 ##this dataframe is only at treatments  
@@ -64,26 +60,26 @@ library(plantecophys)
 ##fitaci with gmes for sun and shade leaves by temperature treatment----------------------------------------------------------
 
 #shade leaves
-fitacishade_at<-fitacis(acishade_clean_at, "chamber", varnames = list(ALEAF="Photo", gmes0 = gm_agg[3,3],
+fitacishade_at<-fitacis(acishade_clean_at, "chamber", varnames = list(ALEAF="Photo", gmes0 = gm_agg[1,3],
                 Tleaf = "Tleaf", Ci="Ci", PPFD="PARi"), Tcorrect=TRUE)
 
-fitacishade_et<-fitacis(acishade_clean_et, "chamber", varnames = list(ALEAF="Photo", gmeso = gm_agg[4,3],
+fitacishade_et<-fitacis(acishade_clean_et, "chamber", varnames = list(ALEAF="Photo", gmeso = gm_agg[2,3],
                 Tleaf = "Tleaf", Ci="Ci", PPFD="PARi"), Tcorrect=TRUE)
 
-fitacishade_redo_at <- fitacis(shade_redo_at, "chamber", varnames = list(ALEAF="Photo", gmes0 = gm_agg[3,3],
+fitacishade_redo_at <- fitacis(shade_redo_at, "chamber", varnames = list(ALEAF="Photo", gmes0 = gm_agg[1,3],
                       Tleaf = "Tleaf", Ci="Ci", PPFD="PARi"), Tcorrect=TRUE)
 
-fitacishade_redo_et <- fitacis(shade_redo_et, "chamber", varnames = list(ALEAF="Photo", gmes0 = gm_agg[4,3],
+fitacishade_redo_et <- fitacis(shade_redo_et, "chamber", varnames = list(ALEAF="Photo", gmes0 = gm_agg[2,3],
                       Tleaf = "Tleaf", Ci="Ci", PPFD="PARi"), Tcorrect=TRUE)
 
 #sun leaves
 fitacitdlaci2 <- fitacis(tdlaci2, "chamber", varnames = list(ALEAF="Photo", gmes0 = gm_agg[5,3],
                              Tleaf = "Tleaf", Ci="Ci", PPFD="PARi"), Tcorrect=TRUE)
 
-fitacisunaci_clean2_at <- fitacis(sunaci_clean2_at, "chamber", varnames = list(ALEAF="Photo", gmes0 = gm_agg[5,3],
+fitacisunaci_clean2_at <- fitacis(sunaci_at, "chamber", varnames = list(ALEAF="Photo", gmes0 = gm_agg[5,3],
                                   Tleaf = "Tleaf", Ci="Ci", PPFD="PARi"), Tcorrect=TRUE)
 
-fitacisunaci_clean2_et <- fitacis(sunaci_clean2_et, "chamber", varnames = list(ALEAF="Photo", gmes0 = gm_agg[6,3],
+fitacisunaci_clean2_et <- fitacis(sunaci_et, "chamber", varnames = list(ALEAF="Photo", gmes0 = gm_agg[6,3],
                                   Tleaf = "Tleaf", Ci="Ci", PPFD="PARi"), Tcorrect=TRUE)
 
 
@@ -118,30 +114,40 @@ chamberorder<-order(jmaxvcmax_gmes$chamber, by=jmaxvcmax_gmes$Vcmax)
 jmaxvcmax_gmes <- jmaxvcmax_gmes[chamberorder,]
 jmaxvcmax_gmes <- merge(jmaxvcmax_gmes, treatments, by = "chamber")
 
+write.csv(jmaxvcmax_gmes, file = "calculated_data/aciparameters_gm.csv", row.names=FALSE)   
+
+
+###before means/stats ch4 sun must be thrown out due to bad curve fitting
+jmaxvcmax_clean <- jmaxvcmax_gmes[!(jmaxvcmax_gmes$chamber == "ch04" & jmaxvcmax_gmes$leaf == "sun"),]
+  
 library(doBy)
 aci_means <- summaryBy(Vcmax+Jmax ~ temp+leaf , data = jmaxvcmax_gmes,  FUN=c(mean,se))
 aci_means2 <- summaryBy(Vcmax+Jmax ~ leaf , data = jmaxvcmax_gmes,  FUN=c(mean,se))
 
 ##compare these to infinite gm parameters
-aci_nogm <- read.csv("calculated_data/aci_sunsha.csv")
+ # aci_nogm <- read.csv("calculated_data/aci_sunsha.csv")
 
 #write to csv
-write.csv(jmax_vcmax, file = "calculated data/jmax_vcmax_gmes.csv", row.names=FALSE)   
+write.csv(jmaxvcmax_clean, file = "calculated_data/jmax_vcmax_gmes.csv", row.names=FALSE) 
 
 
 ##run some stats on jmax, vcmax----------------------------------------------------------------------------------------
-jmaxvcmax_gmes$tukeyid <- as.factor(paste(jmaxvcmax_gmes$leaf, jmaxvcmax_gmes$temp, sep="-"))
+
+
+##can use these but confirm whether curves 4,6 from sun are bad
+
+jmaxvcmax_clean$tukeyid <- as.factor(paste(jmaxvcmax_clean$leaf, jmaxvcmax_clean$temp, sep="-"))
 library(visreg)
 library(multcomp)
 
-J_temp <- lme(Jmax ~ temp ,random=~1|chamber, data=jmaxvcmax_gmes) ##no warming effect
+J_temp <- lme(Jmax ~ temp ,random=~1|chamber, data=jmaxvcmax_clean) ##no warming effect
 summary(J_temp)
 anova(J_temp)
 visreg(J_temp)
 
 ##full model
 
-J_leaf <- lme(Jmax~ tukeyid, random=~1|chamber, data=jmaxvcmax_gmes)
+J_leaf <- lme(Jmax~ tukeyid, random=~1|chamber, data=jmaxvcmax_clean)
 summary(J_leaf)
 anova(J_leaf)
 visreg(J_leaf)
@@ -150,17 +156,19 @@ tukey_Jmax<- glht(J_leaf, linfct = mcp(tukeyid = "Tukey"))
 Jmax_siglets<- cld(tukey_Jmax)
 Jmax_siglets2 <- Jmax_siglets$mcletters$Letters
 
+write.csv(Jmax_siglets2, "master_scripts/sigletters/slr_jmaxgm.csv", row.names=FALSE)
+
 
 #2: Vcmax: no effect of ET
 
-vc_temp <- lme(Vcmax ~ temp ,random=~1|chamber, data=jmaxvcmax_gmes)
+vc_temp <- lme(Vcmax ~ temp ,random=~1|chamber, data=jmaxvcmax_clean)
 summary(vc_temp)
 anova(vc_temp)
 visreg(vc_temp)
 
 ##full model
 
-vc_leaf <- lme(Vcmax~ tukeyid, random=~1|chamber, data=jmaxvcmax_gmes)
+vc_leaf <- lme(Vcmax~ tukeyid, random=~1|chamber, data=jmaxvcmax_clean)
 summary(vc_leaf)
 anova(vc_leaf)
 visreg(vc_leaf)
@@ -168,4 +176,6 @@ visreg(vc_leaf)
 tukey_vcmax<- glht(vc_leaf, linfct = mcp(tukeyid = "Tukey"))
 vcmax_siglets<- cld(tukey_vcmax)
 vcmax_siglets2 <- vcmax_siglets$mcletters$Letters
+
+write.csv(vcmax_siglets2, "master_scripts/sigletters/slr_vcmaxgm.csv", row.names=FALSE)
 
