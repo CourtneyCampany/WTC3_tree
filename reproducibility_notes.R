0d853a1cad06b0a73a58f27adf7cbb204a24d07d
Courtney Campany <courtneycampany@gmail.com>
2016-06-17 10:29:44 +1000

    cleaning repo

--- reproducibility_notes.R
+++ reproducibility_notes.R
@@ -3,11 +3,32 @@
 
 ###between thesis and pub
 
-#1. I saved the figures and tables in a secondary RMD, maybe easier to convert back from thesis (which are inbedded)
 #2. Figure #s in thesis inbedded have been re# with thesis chapter Figure #.#
 #3. Likely will need to use pdf figures anyway, but then a list of figure tables is available.
-
 #4. for RMD will use two dummy docx files for thesis and pub, thesis will need to reflect binding requirements.
+
+###notes on manuscrupt to thesis document
+
+###figures----------------------------------------------------------------------------------------------------------------
+
+#1. changed physiology bar plots to box plots (includes changes to caption)
+#2. changed drawdown bar plots to box plots (includes changes to caption)
+#3. replaced "respone to" to "relationship between" for several figures
+#4. added gmshade figure (as fig 6), renumbered figure 6to7
+
+
+###universal---------------------------------------------------------------------------------------------------------------
+
+#1. units on ppm changed to ubar
+#2. units on gm changed to included bar
+
+
+
+##introduction-------------------------------------------------------------------------------------------------------------
+#1. added hanba and terashima to Boardman cite, sunfleck paragraph
+#2. deleted hyptotheses and rewrote last paragraph
+#3. added Buckely cite to N to light sentence
+
 
 #comment out keywords for thesis
 #comment out author correspondence for thesis
