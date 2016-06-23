243bf28c2fd07394c6d603197a5100acf7c7ccb9
Remko Duursma <remkoduursma@gmail.com>
2016-06-23 11:23:29 +1000

    More edits discussion and figure captions; split supp info into standalone doc; preloading stuff into separate script

---
+++ gm_newphyt/manuscript_preload.R
@@ -0,0 +1,16 @@
+
+
+r <- require(pacman)
+if(!r)install.packages("pacman")
+pacman::p_load(broom,reporttools,lubridate, doBy, plyr,plotrix, wesanderson, plantecophys, scales,reshape, pixiedust)
+
+
+source("functions and packages/functions.R")
+source("master_scripts/plot_objects.R")
+
+##read table ready dataframes
+resourcetab <- read.csv("master_scripts/resource_table.csv", stringsAsFactors = FALSE)
+getab <- read.csv("master_scripts/ge_table.csv", stringsAsFactors = FALSE)
+pval <- c(0.781, 0.001, 0.028, 0.002, 0.973, 0.3486, 0.6385, 0.001)
+pval2 <- c(0.001,	0.001,	0.001,	0.001,	0.001,	0.005,	0.001,	0.001)
+
