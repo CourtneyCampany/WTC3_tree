5d32edbdfa3ad91dbdcf1d7783123e8654e8a0db
Courtney Campany <courtneycampany@gmail.com>
2016-08-31 13:28:34 +1000

    testing e

--- functions and packages/gmes_functions.R
+++ functions and packages/gmes_functions.R
@@ -157,8 +157,10 @@ gmesdata_func <- function(xsi_dfr, licor_dfr, times_dfr, licorrows=5, whichlicor
 }
 
 ####gmescalculation----------------------------------------------------------------------------------------------
-gmcalc_func <- function(x, a=4.4, ab= 2.9, e=30, b=29, f=16.2,del_growth = -8 , delR=-38, 
+gmcalc_func <- function(x, a=4.4, ab= 2.9, b=29, f=16.2,del_growth = -8 , delR=-38, 
                         k25r=0.728, k25g=38.89, Ea_r = 72.311, Ea_g = 20.437,Rgc=8.314472){
+  
+  e = del_growth - delR
   
   x$CiCa <- x$Ci/x$CO2R
   x$a_prime <- (ab*(x$CO2S-x$C2sfc)+a*(x$C2sfc-x$Ci))/(x$CO2S-x$Ci)
@@ -251,3 +253,38 @@ gmcalc_func2 <- function(x, a=4.4, ab= 2.9, e=30, b=29, f=16.2,del_growth = -8 ,
 }
 
 
+### new e
+gmcalc_func3 <- function(x, a=4.4, ab= 2.9, b=29, f=16.2,del_growth = -8 , delR=-5, 
+                        k25r=0.728, k25g=38.89, Ea_r = 72.311, Ea_g = 20.437,Rgc=8.314472){
+  
+  e = del_growth - delR
+  
+  x$CiCa <- x$Ci/x$CO2R
+  x$a_prime <- (ab*(x$CO2S-x$C2sfc)+a*(x$C2sfc-x$Ci))/(x$CO2S-x$Ci)
+  
+  x$Rd <- k25r * exp(Ea_r*((x$Tleaf+273.15)-298)/(298*Rgc*(x$Tleaf+273.15)))
+  x$Gstar <- k25g * exp(Ea_g*((x$Tleaf+273.15)-298)/(298*Rgc*(x$Tleaf+273.15)))
+  
+  x$rd_term <- e*x$Rd*(x$Ci-x$Gstar)/((x$Photo+x$Rd)*x$CO2S)
+  x$f_term <- f*x$Gstar/x$CO2S
+  
+  x$TleafminusTair <- x$Tleaf - x$Tair
+  x$TblockminusTair <- x$TBlk - x$Tair
+  
+  x$CO2Rdry <- x$CO2R/(1-x$H2OR/1000)
+  x$CO2Sdry <- x$CO2S/(1-x$H2OS/1000)
+  
+  x$t <- (1+x$a_prime/1000)*x$Trmmol/x$CndCO2/1000/2
+  x$t2 <- 1/(1-x$t)
+  x$t3 <- (1+x$t)/(1-x$t)
+  
+  x$Di <- x$a_prime * x$t2+ (x$t3 * b-x$a_prime * x$t2) * x$CiCa
+  x$DiminusDo <- x$Di - x$DELTA
+  
+  x$rd_term2 <- x$t3- x$rd_term
+  x$f_term2 <- x$t3 - x$f_term
+  
+  x$gm <- x$t3 * (b - 1.8 - x$Rd * e / (x$Rd+x$Photo)) * x$Photo/x$CO2S/(x$DiminusDo - x$rd_term2 - x$f_term2)
+  x$gm_bar <- x$gm*100/x$Press
+  return(x)
+}
--- scripts/fulldelta_eq.R
+++
@@ -1,7 +0,0 @@
-$$
-  \begin{aligned}
-\Delta = &\frac{1}{1-t}a'+\frac{1}{1-t}((1+t)b-a')\frac{C_i}{C_a}\\
-&\frac{1+t}{1-t}\left(b-a'-\frac{eR_d}{A+R_d}\right)\frac{A}{g_mC_a}-\\
-                      &\frac{1+t}{1-t}\left(\frac{eR_D}{(A+R_d)C_a}(C_i-\Gamma_*)\right)- \frac{1+t}{1-t}\left(f\frac{\Gamma_*}{C_a}\right)\\
-                      \end{aligned}
-                      $$
